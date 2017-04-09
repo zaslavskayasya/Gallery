cd29ecee51a9fb66e9c1afdedb9f9e4d54b2776e
User <User>
2017-04-09 14:46:10 +0300

    service

--- controllers/popularController.js
+++ controllers/popularController.js
@@ -1,75 +1,7 @@
-app.controller('popularController', function ($scope, $rootScope, $routeParams,  $localStorage) {
-        var popIndex = 0;
+app.controller('popularController', function ($scope, $rootScope, $routeParams, pictures,  $localStorage) {
+    var popIndex = 0;
 
-       $scope.popItem = [
-           {
-               id: 1,
-               image : './img/img.jpg',
-               imageUrl : './img/img',
-               name : 'Весеннее настроение1',
-               size : '40 X 50',
-               materials: 'масло, холст',
-               description: 'lorem lalala'
-
-           },
-           {
-               id: 2,
-               image : 'slider.jpg',
-               name : 'Весеннее настроение2',
-               size : '40 X 50',
-               materials: 'масло, холст',
-               description: 'lorem lalala'
-           },
-           {
-               id: 3,
-               image : 'slider.jpg',
-               name : 'Весеннее настроение3',
-               size : '40 X 50',
-               materials: 'масло, холст',
-               description: 'lorem kkks wejknf awkjenf jeknfjawdbv ajkwbfkjabwefjksdb asfvaer'
-           },
-           {
-               id: 4,
-               image : 'slider.jpg',
-               name : 'Весеннее настроение4',
-               size : '40 X 50',
-               materials: 'масло, холст',
-               description: 'lorem kkks wejknf awkjenf jeknfjawdbv ajkwbfkjabwefjksdb asfvaer'
-           },
-           {
-               id: 5,
-               image : 'slider.jpg',
-               name : 'Весеннее настроение5',
-               size : '40 X 50',
-               materials: 'масло, холст',
-               description: 'lorem kkks wejknf awkjenf jeknfjawdbv ajkwbfkjabwefjksdb asfvaer'
-
-           },
-           {
-               id: 6,
-               image : 'slider.jpg',
-               name : 'Весеннее настроение6',
-               size : '40 X 50',
-               materials: 'масло, холст',
-               description: 'lorem kkks wejknf awkjenf jeknfjawdbv ajkwbfkjabwefjksdb asfvaer'
-           },
-           {
-               id: 7,
-               image : 'slider.jpg',
-               name : 'Весеннее настроение7',
-               size : '40 X 50',
-               materials: 'масло, холст',
-               description: 'lorem kkks wejknf awkjenf jeknfjawdbv ajkwbfkjabwefjksdb asfvaer'
-           },
-           {
-               id: 8,
-               image : 'slider.jpg',
-               name : 'Весеннее настроение8',
-               size : '40 X 50',
-               materials: 'масло, холст',
-               description: 'lorem kkks wejknf awkjenf jeknfjawdbv ajkwbfkjabwefjksdb asfvaer'
-           },
-       ];
+    $scope.popItem = pictures.pictures;
 
     $scope.send = function (name, id, image , size, materials, description) {
         // $broadcast - отправка события всем scope от rootScope
---
+++ services/picturesService.js
@@ -0,0 +1,147 @@
+app.factory('pictures', function() {
+    return {
+        pictures: [
+            {
+                id: 1,
+                image : './img/img.jpg',
+                name : 'Весеннее настроение1',
+                size : '40 X 50',
+                materials: 'масло, холст',
+                description: 'коты',
+                popular: true,
+
+            },
+            {
+                id: 2,
+                image : './img/img.jpg',
+                name : 'Весеннее настроение1',
+                size : '30 X 50',
+                materials: 'frdfhtkm, холст',
+                description: 'люди',
+                popular: true
+
+            },
+            {
+                id: 3,
+                image : './img/img.jpg',
+                name : 'Весеннее настроение1',
+                size : '30 X 50',
+                materials: 'frdfhtkm, холст',
+                description: 'пейзаж',
+                popular: true
+
+            },
+            {
+                id: 4,
+                image : './img/img.jpg',
+                name : 'Весеннее настроение1',
+                size : '30 X 50',
+                materials: 'frdfhtkm, холст',
+                description: 'цветы',
+                popular: true
+
+            },
+            {
+                id: 5,
+                image : './img/img.jpg',
+                name : 'Весеннее настроение1',
+                size : '30 X 50',
+                materials: 'frdfhtkm, холст',
+                description: 'Одесса',
+                popular: true
+
+            },
+            {
+                id: 6,
+                image : './img/img.jpg',
+                imageUrl : './img/img',
+                name : 'Весеннее настроение1',
+                size : '40 X 50',
+                materials: 'масло, холст',
+                description: 'коты',
+                popular: true
+
+            },
+            {
+                id: 7,
+                image : './img/img.jpg',
+                name : 'Весеннее настроение1',
+                size : '30 X 50',
+                materials: 'frdfhtkm, холст',
+                description: 'люди',
+                popular: true
+
+            },
+            {
+                id: 8,
+                image : './img/img.jpg',
+                name : 'Весеннее настроение1',
+                size : '30 X 50',
+                materials: 'frdfhtkm, холст',
+                description: 'пейзаж',
+                popular: true
+
+            },
+            {
+                id: 9,
+                image : './img/img.jpg',
+                name : 'Весеннее настроение1',
+                size : '30 X 50',
+                materials: 'frdfhtkm, холст',
+                description: 'Одесса',
+                popular: true
+
+            },
+            {
+                id: 10,
+                image : './img/img.jpg',
+                name : 'Весеннее настроение1',
+                size : '30 X 50',
+                materials: 'frdfhtkm, холст',
+                description: 'Одесса',
+                popular: true
+
+            },
+            {
+                id: 11,
+                image : './img/img.jpg',
+                name : 'Весеннее настроение1',
+                size : '30 X 50',
+                materials: 'frdfhtkm, холст',
+                description: 'Одесса',
+                popular: true
+
+            },
+            {
+                id: 12,
+                image : './img/img.jpg',
+                name : 'Весеннее настроение1',
+                size : '30 X 50',
+                materials: 'frdfhtkm, холст',
+                description: 'Одесса',
+                popular: false
+
+            },
+            {
+                id: 13,
+                image : './img/img.jpg',
+                name : 'Весеннее настроение1',
+                size : '30 X 50',
+                materials: 'frdfhtkm, холст',
+                description: 'Одесса',
+                popular: false
+
+            },
+            {
+                id: 14,
+                image : './img/img.jpg',
+                name : 'Весеннее настроение1',
+                size : '30 X 50',
+                materials: 'frdfhtkm, холст',
+                description: 'коты',
+                popular: true
+
+            },
+        ]
+    }
+});
